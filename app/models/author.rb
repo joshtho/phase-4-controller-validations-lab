321044173589a609f6efb853074095ebddeb6d25
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-01-28 10:07:09 -0600

    commit tree 117eaedf34d8245c067ea3473783562bbc0c82b5

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
-    validates :name, presence: true, uniqueness: true 
+    validates :name, presence: true, uniqueness: true
     validates :email, uniqueness: true 
 end
